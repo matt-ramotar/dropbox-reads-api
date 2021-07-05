48322e1f86806b3f3a151b15162a1207151f9a3a
Matt Ramotar <mramotar@dropbox.com>
2021-07-05 09:03:46 -0400

    Add ContinueWithGoogleSuccess response

---
+++ src/api/auth/entities/responses.ts
@@ -0,0 +1,16 @@
+import { SafeUser } from "../../users/models/SafeUser";
+
+export interface SuccessfulAuth {
+  user: SafeUser;
+  token: string;
+}
+
+export class ContinueWithGoogleSuccess implements SuccessfulAuth {
+  readonly user: SafeUser;
+  readonly token: string;
+
+  constructor(user: SafeUser, token: string) {
+    this.user = user;
+    this.token = token;
+  }
+}
