8f8c448773bba185b9e9d2484aa7485ef62c16e8
Matt Ramotar <mramotar@dropbox.com>
2021-07-05 09:02:51 -0400

    Remove createUser mutation

--- src/api/users/mutations/UserMutation.ts
+++ src/api/users/mutations/UserMutation.ts
@@ -1,18 +1,3 @@
-import { UserModel } from "../../../models";
-import User from "../models/User";
+interface UserMutation {}
 
-interface UserMutation {
-  createUser(firstName: string, lastName: string, email: string, username: string, password: string): Promise<User>;
-}
-
-export default class RealUserMutation implements UserMutation {
-  public async createUser(
-    firstName: string,
-    lastName: string,
-    email: string,
-    username: string,
-    password: string
-  ): Promise<User> {
-    return await UserModel.create({ firstName, lastName, email, username, password });
-  }
-}
+export default class RealUserMutation implements UserMutation {}
