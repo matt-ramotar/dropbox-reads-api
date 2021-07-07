474a45f2c5f4e22b27e1bbe8a5ec08ced273d738
Matt Ramotar <mramotar@dropbox.com>
2021-07-07 12:14:23 -0400

    Add service that creates book

---
+++ src/api/books/services/createBook.ts
@@ -0,0 +1,31 @@
+import { AuthorModel, BookModel, UserModel } from "../../../models";
+import { CreateBookInput } from "../entities/CreateBookInput";
+import Book from "../models/Book";
+
+export default async function createBook(input: CreateBookInput): Promise<Book> {
+  try {
+    const { googleId, title, coverImage, author: authorId, tags: tagIds, userAddedBy: userId } = input;
+
+    const user = await UserModel.findById(userId);
+    if (!user) throw new Error();
+
+    const author = await AuthorModel.findById(authorId);
+    if (!author) throw new Error();
+
+    const book = await BookModel.create({
+      googleId,
+      title,
+      coverImage,
+      author: author.id,
+      tags: tagIds,
+      userAddedBy: user.id
+    });
+
+    if (user.booksAdded) user.booksAdded.push(book.id);
+    else user.booksAdded = [book.id];
+
+    return book;
+  } catch (error) {
+    throw error;
+  }
+}
