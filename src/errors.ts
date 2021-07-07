4482dcc40bc9908d8af20fc2841b4cc8b8b4ea42
Matt Ramotar <mramotar@dropbox.com>
2021-07-07 13:56:43 -0400

    Add errors

---
+++ src/errors.ts
@@ -0,0 +1,124 @@
+export interface DropboxReadsError {
+  message?: string;
+  error?: any;
+}
+
+export class AuthorNotFound implements DropboxReadsError {
+  readonly message?: string = "Author not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class BookNotFound implements DropboxReadsError {
+  readonly message?: string = "Book not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class BookshelfNotFound implements DropboxReadsError {
+  readonly message?: string = "Bookshelf not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class BookTagNotFound implements DropboxReadsError {
+  readonly message?: string = "BookTag not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class BookTagUpvoteNotFound implements DropboxReadsError {
+  readonly message?: string = "BookTagUpvote not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class ReactionNotFound implements DropboxReadsError {
+  readonly message?: string = "Reaction not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class ReviewReactionNotFound implements DropboxReadsError {
+  readonly message?: string = "ReviewReaction not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class ReviewNotFound implements DropboxReadsError {
+  readonly message?: string = "Review not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class ReviewUpvoteNotFound implements DropboxReadsError {
+  readonly message?: string = "ReviewUpvote not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class RoleNotFound implements DropboxReadsError {
+  readonly message?: string = "Role not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class TagNotFound implements DropboxReadsError {
+  readonly message?: string = "Tag not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
+
+export class UserNotFound implements DropboxReadsError {
+  readonly message?: string = "User not found";
+  readonly error?: any;
+
+  constructor(message?: string, error?: any) {
+    this.message = message;
+    this.error = error;
+  }
+}
