edb17bb343865893ddcc813f7e7fefab3ad5c157
Matt Ramotar <mramotar@dropbox.com>
2021-07-07 17:11:00 -0400

    Add ActionType enum

---
+++ src/api/actions/models/ActionType.ts
@@ -0,0 +1,15 @@
+enum ActionType {
+  AddBookToBookshelf = "ADD BOOK TO BOOKSHELF",
+  AddTagToBook = "ADD TAG TO BOOK",
+  AddCommentToReview = "ADD COMMENT TO REVIEW",
+  CreateBook = "CREATE BOOK",
+  CreateBookshelf = "CREATE BOOKSHELF",
+  CreateReview = "CREATE REVIEW",
+  FollowUser = "FOLLOW USER",
+  FollowTag = "FOLLOW TAG",
+  ReactToComment = "REACT TO COMMENT",
+  ReactToReview = "REACT TO REVIEW",
+  UpvoteBookTag = "UPVOTE BOOK TAG",
+  UpvoteComment = "UPVOTE COMMENT",
+  UpvoteReview = "UPVOTE REVIEW"
+}
