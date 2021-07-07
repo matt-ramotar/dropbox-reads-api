9f9ed367fbc27c85cfcd3fa6ca4ec6cd8b7829b3
Matt Ramotar <mramotar@dropbox.com>
2021-07-06 22:08:51 -0400

    Add Bookshelf model

---
+++ src/api/bookshelves/models/Bookshelf.ts
@@ -0,0 +1,36 @@
+import { prop, Ref } from "@typegoose/typegoose";
+import { Field, ID, ObjectType } from "type-graphql";
+import Book from "../../books/models/Book";
+import Tag from "../../tags/models/Tag";
+import User from "../../users/models/User";
+
+/**
+ * @tsoaModel
+ */
+
+@ObjectType({ description: "Bookshelf model" })
+export default class Bookshelf {
+  @Field(() => ID)
+  @prop({ ref: () => Bookshelf })
+  id!: string;
+
+  @Field()
+  @prop()
+  name!: string;
+
+  @Field()
+  @prop()
+  description!: string;
+
+  @Field(() => ID)
+  @prop({ ref: () => Book })
+  books?: Ref<Book, string>;
+
+  @Field(() => ID)
+  @prop({ ref: () => User })
+  owner!: Ref<User, string>;
+
+  @Field(() => ID)
+  @prop({ ref: () => Tag })
+  tags?: Ref<Tag, string>;
+}
--- src/models.ts
+++ src/models.ts
@@ -1,6 +1,7 @@
 import { getModelForClass } from "@typegoose/typegoose";
 import Author from "./api/authors/models/Author";
 import Book from "./api/books/models/Book";
+import Bookshelf from "./api/bookshelves/models/Bookshelf";
 import BookTag from "./api/booktags/models/BookTag";
 import BookTagUpvote from "./api/booktagupvotes/models/BookTagUpvote";
 import Tag from "./api/tags/models/Tag";
@@ -8,6 +9,7 @@ import User from "./api/users/models/User";
 
 export const AuthorModel = getModelForClass(Author);
 export const BookModel = getModelForClass(Book);
+export const BookshelfModel = getModelForClass(Bookshelf);
 export const BookTagModel = getModelForClass(BookTag);
 export const BookTagUpvoteModel = getModelForClass(BookTagUpvote);
 export const TagModel = getModelForClass(Tag);
