adda99677709b885aa129fb619b1a9ff40bbe3d0
Matt Ramotar <mramotar@dropbox.com>
2021-07-06 21:34:43 -0400

    Add Book model

---
+++ src/api/books/models/Book.ts
@@ -0,0 +1,39 @@
+import { prop, Ref } from "@typegoose/typegoose";
+import { Field, ID, ObjectType } from "type-graphql";
+import Author from "../../authors/models/Author";
+import User from "../../users/models/User";
+
+/**
+ * @tsoaModel
+ */
+
+@ObjectType({ description: "Book model" })
+export default class Book {
+  @Field(() => ID)
+  @prop({ ref: () => Book })
+  id!: string;
+
+  @Field()
+  @prop()
+  googleId!: string;
+
+  @Field()
+  @prop()
+  title!: string;
+
+  @Field()
+  @prop()
+  coverImage?: string;
+
+  @Field(() => ID)
+  @prop({ ref: () => Author })
+  author!: Ref<Author, string>;
+
+  @Field(() => ID)
+  @prop({ ref: () => Tag })
+  tags?: Ref<Tag, string>[];
+
+  @Field(() => ID)
+  @prop({ ref: () => User })
+  userAddedBy!: Ref<User, string>;
+}
--- src/models.ts
+++ src/models.ts
@@ -1,6 +1,8 @@
 import { getModelForClass } from "@typegoose/typegoose";
 import Author from "./api/authors/models/Author";
+import Book from "./api/books/models/Book";
 import User from "./api/users/models/User";
 
 export const AuthorModel = getModelForClass(Author);
+export const BookModel = getModelForClass(Book);
 export const UserModel = getModelForClass(User);
