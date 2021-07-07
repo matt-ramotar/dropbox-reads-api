e5d8b665de410d9920b6ad26867737b82d7fd70c
Matt Ramotar <mramotar@dropbox.com>
2021-07-07 12:42:03 -0400

    Add service that creates author

---
+++ src/api/authors/services/createAuthor.ts
@@ -0,0 +1,13 @@
+import { AuthorModel } from "../../../models";
+import { CreateAuthorInput } from "../entities/CreateAuthorInput";
+import Author from "../models/Author";
+
+export default async function createAuthor(input: CreateAuthorInput): Promise<Author> {
+  try {
+    const { firstName, lastName } = input;
+
+    return await AuthorModel.create({ firstName, lastName });
+  } catch (error) {
+    throw error;
+  }
+}
