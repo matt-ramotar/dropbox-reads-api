0c5aef2f4b2c0f28685c7e39afd3dba972013d9b
Matt Ramotar <mramotar@dropbox.com>
2021-07-07 11:01:45 -0400

    Add Review model

---
+++ src/api/reviews/models/Review.ts
@@ -0,0 +1,35 @@
+import { prop, Ref } from "@typegoose/typegoose";
+import { Field, ID, ObjectType } from "type-graphql";
+import Book from "../../books/models/Book";
+import User from "../../users/models/User";
+
+/**
+ * @tsoaModel
+ */
+
+@ObjectType({ description: "Review model" })
+export default class Review {
+  @Field(() => ID)
+  @prop({ ref: () => Review })
+  id!: string;
+
+  @Field(() => ID)
+  @prop({ ref: () => User })
+  reviewer!: Ref<User, string>;
+
+  @Field(() => ID)
+  @prop({ ref: () => Book })
+  book!: Ref<Book, string>;
+
+  @Field(() => ID)
+  @prop({ ref: () => ReviewUpvote })
+  upvotes?: Ref<ReviewUpvote, string>[];
+
+  @Field()
+  @prop()
+  rating!: number;
+
+  @Field()
+  @prop()
+  body!: string;
+}
