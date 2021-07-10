6fd2241d409ea5501181c2cd5ea7a7ff528bde76
Matt Ramotar <mramotar@dropbox.com>
2021-07-10 19:07:33 -0400

    Add service that gets action by ID

--- src/api/actions/services/ActionService.ts
+++ src/api/actions/services/ActionService.ts
@@ -1,13 +1,20 @@
 import { CreateActionInput } from "../entities/CreateActionInput";
 import Action from "../models/Action";
+import { GodAction } from "../models/GodAction";
 import createAction from "./createAction";
+import getActionById from "./getActionById";
 
 interface ActionService {
   createAction(input: CreateActionInput): Promise<Action>;
+  getActionById(actionId: string): Promise<GodAction>;
 }
 
 export default class RealActionService implements ActionService {
   public async createAction(input: CreateActionInput): Promise<Action> {
     return await createAction(input);
+  }
+
+  public async getActionById(actionId: string): Promise<GodAction> {
+    return await getActionById(actionId);
   }
 }
---
+++ src/api/actions/services/getActionById.ts
@@ -0,0 +1,15 @@
+import { ActionNotFound } from "../../../errors";
+import { ActionModel } from "../../../models";
+import { GodAction } from "../models/GodAction";
+
+export default async function getActionById(actionId: string): Promise<GodAction> {
+  try {
+    const action = await ActionModel.findById(actionId);
+    if (!action) throw new ActionNotFound();
+    console.log(action);
+
+    return await action.toGodAction();
+  } catch (error) {
+    throw error;
+  }
+}
