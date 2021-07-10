7fd99657e84a9c6c11ad8085d450f5fde530d6ff
Matt Ramotar <mramotar@dropbox.com>
2021-07-10 19:06:56 -0400

    Add Refs entity

---
+++ src/api/actions/entities/Refs.ts
@@ -0,0 +1,13 @@
+export interface Refs {
+  userId?: string;
+  otherUserId?: string;
+  bookId?: string;
+  bookshelfId?: string;
+  bookTagId?: string;
+  tagId?: string;
+  reviewId?: string;
+  commentId?: string;
+  otherCommentId?: string;
+  reviewReactionId?: string;
+  commentReactionId?: string;
+}
