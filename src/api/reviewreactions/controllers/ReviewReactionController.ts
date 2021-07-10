b8098b09582c0c766ef9c2efafe7165b83a6f668
Matt Ramotar <mramotar@dropbox.com>
2021-07-10 10:07:10 -0400

    Add ReviewReaction controller

---
+++ src/api/reviewreactions/controllers/ReviewReactionController.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Post, Route, Tags } from "tsoa";
+import { ActionType } from "../../actions/models/ActionType";
+import RealActionService from "../../actions/services/ActionService";
+import RealReactionService from "../../reactions/services/ReactionService";
+import RealUserService from "../../users/services/UserService";
+import { CreateReviewReactionInput } from "../entities/CreateReviewReactionInput";
+import ReviewReaction from "../models/ReviewReaction";
+import RealReviewReactionService from "../services/ReviewReactionService";
+
+@Route("reviewreactions")
+@Tags("ReviewReaction")
+export class ReviewReactionController extends Controller {
+  /** React to review */
+  @Post()
+  async createReviewReaction(@Body() input: CreateReviewReactionInput): Promise<ReviewReaction> {
+    const { reviewId, userId, reaction: reactionInput } = input;
+
+    const actionService = new RealActionService();
+    const reactionService = new RealReactionService();
+    const reviewReactionService = new RealReviewReactionService();
+    const userService = new RealUserService();
+
+    const reaction = await reactionService.upsertReaction(reactionInput);
+    const reviewReaction = await reviewReactionService.createReviewReaction(reviewId, userId, reaction.id);
+
+    const action = await actionService.createAction({
+      type: ActionType.ReactToReview,
+      userId,
+      reviewId,
+      reviewReactionId: reviewReaction.id
+    });
+
+    await userService.addAction(action.id, userId);
+    await userService.publishAction(action.id, userId);
+
+    return reviewReaction;
+  }
+}
