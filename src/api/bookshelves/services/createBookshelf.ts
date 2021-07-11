a23bd474edae8cd1e9167d32c4507d0e7764c856
Matt Ramotar <mramotar@dropbox.com>
2021-07-11 19:02:30 -0400

    Fix param name

--- src/api/bookshelves/services/createBookshelf.ts
+++ src/api/bookshelves/services/createBookshelf.ts
@@ -1,12 +1,12 @@
 import { BookshelfModel } from "../../../models";
 import Bookshelf from "../models/Bookshelf";
 
-export default async function createBookshelf(name: string, description: string, ownerId: string, tagIds: string[]): Promise<Bookshelf> {
+export default async function createBookshelf(name: string, description: string, userId: string, tagIds: string[]): Promise<Bookshelf> {
   try {
     return await BookshelfModel.create({
       name,
       description,
-      userId: ownerId,
+      userId,
       tagIds
     });
   } catch (error) {
