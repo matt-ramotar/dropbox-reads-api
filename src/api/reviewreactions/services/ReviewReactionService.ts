5eb46ef8fd3215d1170eaef2a5528fa5c0d0ee89
Matt Ramotar <mramotar@dropbox.com>
2021-07-11 20:06:29 -0400

    Use DocumentType wrapper

--- src/api/reviewreactions/services/ReviewReactionService.ts
+++ src/api/reviewreactions/services/ReviewReactionService.ts
@@ -1,12 +1,13 @@
+import { DocumentType } from "@typegoose/typegoose";
 import ReviewReaction from "../models/ReviewReaction";
 import createReviewReaction from "./createReviewReaction";
 
 interface ReviewReactionService {
-  createReviewReaction(reviewId: string, userId: string, reactionId: string): Promise<ReviewReaction>;
+  createReviewReaction(reviewId: string, userId: string, reactionId: string): Promise<DocumentType<ReviewReaction>>;
 }
 
 export default class RealReviewReactionService implements ReviewReactionService {
-  public async createReviewReaction(reviewId: string, userId: string, reactionId: string): Promise<ReviewReaction> {
+  public async createReviewReaction(reviewId: string, userId: string, reactionId: string): Promise<DocumentType<ReviewReaction>> {
     return await createReviewReaction(reviewId, userId, reactionId);
   }
 }
--- src/api/reviewreactions/services/createReviewReaction.ts
+++ src/api/reviewreactions/services/createReviewReaction.ts
@@ -1,7 +1,12 @@
+import { DocumentType } from "@typegoose/typegoose";
 import { ReviewReactionModel } from "../../../models";
 import ReviewReaction from "../models/ReviewReaction";
 
-export default async function createReviewReaction(reviewId: string, userId: string, reactionId: string): Promise<ReviewReaction> {
+export default async function createReviewReaction(
+  reviewId: string,
+  userId: string,
+  reactionId: string
+): Promise<DocumentType<ReviewReaction>> {
   try {
     return await ReviewReactionModel.create({
       reviewId,
