9edc82ab55189b6c2d454e490a527dd3b79afbea
Dana Lu <dlu200774@gmail.com>
2021-07-13 15:32:30 -0400

    Validate upvote fields before creating new one

--- src/api/bookupvotes/services/createBookUpvote.ts
+++ src/api/bookupvotes/services/createBookUpvote.ts
@@ -1,9 +1,14 @@
 import BookUpvote from "../../bookupvotes/models/BookUpvote";
 import { BookUpvoteModel } from "../../../models";
 import { DocumentType } from "@typegoose/typegoose";
+import { RelationshipAlreadyExists } from "../../../errors";
 
 export default async function createBookUpvote(bookId: string, userId: string): Promise<DocumentType<BookUpvote>> {
     try {
+        if (await BookUpvoteModel.exists({bookId: bookId, userId: userId})) {
+            throw RelationshipAlreadyExists;
+        }
+        
         return await BookUpvoteModel.create({
             bookId,
             userId,
