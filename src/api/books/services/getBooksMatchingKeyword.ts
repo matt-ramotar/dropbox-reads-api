996c49f04104756e9d2c8b8ddc28f8a03e633e2e
Dana Lu <dlu200774@gmail.com>
2021-07-14 15:58:52 -0400

    Move comment to relevant location

--- src/api/books/services/getBooksMatchingKeyword.ts
+++ src/api/books/services/getBooksMatchingKeyword.ts
@@ -2,6 +2,7 @@ import { BookModel } from "../../../models";
 import { GodBook } from "../../books/models/GodBook";
 
 export default async function getBooksMatchingKeyword(keyword: string): Promise<GodBook[]> {
+    // Search in book title, book author, and book tag fields
     const matchingTitles = await BookModel.find({title: {$regex: keyword, $options: 'i'}});
     const books = [];
     for (const title of matchingTitles) {
--- src/api/search/controllers/SearchController.ts
+++ src/api/search/controllers/SearchController.ts
@@ -5,7 +5,7 @@ import { GodBook } from "../../books/models/GodBook";
 @Route("search")
 @Tags("Search")
 export class SearchController extends Controller {
-  /** Search in book author, title, and tag fields */
+    
   @Get("{keyword}")
   async search(@Path() keyword: string): Promise<GodBook[]> {
     return await new RealBookService().getBooksMatchingKeyword(keyword);
