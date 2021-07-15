42f1a6ec5738bc8e9615451329ebbf5ce5024c2c
Matt Ramotar <mramotar@dropbox.com>
2021-07-14 23:02:12 -0400

    Fix CommentReaction routes

--- src/api/commentreactions/services/createCommentReaction.ts
+++ src/api/commentreactions/services/createCommentReaction.ts
@@ -3,11 +3,13 @@ import CommentReaction from "../models/CommentReaction";
 
 export default async function createCommentReaction(commentId: string, userId: string, reactionId: string): Promise<CommentReaction> {
   try {
-    return await CommentReactionModel.create({
+    const commentReaction = await CommentReactionModel.create({
       commentId,
       userId,
       reactionId
     });
+
+    return await commentReaction.toPojo();
   } catch (error) {
     throw error;
   }
