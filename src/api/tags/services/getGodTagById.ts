5560b00c9de6c4d4dec50e27f1b75d65673353cb
Dana Lu <dlu200774@gmail.com>
2021-07-15 11:06:55 -0400

    remove console logs

--- src/api/tags/services/getGodTagById.ts
+++ src/api/tags/services/getGodTagById.ts
@@ -5,14 +5,12 @@ import { GodTag } from "../models/GodTag";
 export default async function getGodTagById(tagId: string): Promise<GodTag> {
   try {
     // Try to find either as tagId or tagName
-    console.log(tagId);
     const tag = await TagModel.findOne({
       $or: [
         {id: tagId},
         {tag: {$regex: tagId, $options: "i"}},
       ]
     });
-    console.log(tag);
     if (!tag) throw new TagNotFound();
     return await tag.toGodTag();
   } catch (error) {
