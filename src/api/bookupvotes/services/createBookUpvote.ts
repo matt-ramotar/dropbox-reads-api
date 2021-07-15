f611714f94f38f66323fddc7e14224da8c3f71e3
Matt Ramotar <mramotar@dropbox.com>
2021-07-14 23:00:28 -0400

    Fix BookUpvote routes

--- src/api/bookupvotes/services/createBookUpvote.ts
+++ src/api/bookupvotes/services/createBookUpvote.ts
@@ -1,19 +1,20 @@
-import BookUpvote from "../../bookupvotes/models/BookUpvote";
-import { BookUpvoteModel } from "../../../models";
-import { DocumentType } from "@typegoose/typegoose";
 import { RelationshipAlreadyExists } from "../../../errors";
+import { BookUpvoteModel } from "../../../models";
+import BookUpvote from "../../bookupvotes/models/BookUpvote";
 
-export default async function createBookUpvote(bookId: string, userId: string): Promise<DocumentType<BookUpvote>> {
-    try {
-        if (await BookUpvoteModel.exists({bookId: bookId, userId: userId})) {
-            throw new RelationshipAlreadyExists();
-        }
-        
-        return await BookUpvoteModel.create({
-            bookId,
-            userId,
-        });
-    } catch (error) {
-        throw error;
+export default async function createBookUpvote(bookId: string, userId: string): Promise<BookUpvote> {
+  try {
+    if (await BookUpvoteModel.exists({ bookId: bookId, userId: userId })) {
+      throw new RelationshipAlreadyExists();
     }
+
+    const bookUpvote = await BookUpvoteModel.create({
+      bookId,
+      userId
+    });
+
+    return await bookUpvote.toPojo();
+  } catch (error) {
+    throw error;
+  }
 }
