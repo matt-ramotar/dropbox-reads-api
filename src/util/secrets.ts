d387a09179fd9f144a5935b41328f4f0495d9d33
Matt Ramotar <mramotar@dropbox.com>
2021-07-15 23:07:00 -0400

    Migrate

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -1,6 +1,6 @@
-import logger from "./logger";
 import dotenv from "dotenv";
 import fs from "fs";
+import logger from "./logger";
 
 if (fs.existsSync(".env")) {
   logger.debug("Using .env file to supply config environment variables.");
@@ -12,3 +12,6 @@ export const MONGODB_URI = process.env["MONGODB_URI"];
 export const KEYS = process.env.SECRET_OR_KEY;
 export const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 export const ROOT_AWS_S3 = process.env.ROOT_AWS_S3;
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+export const DBX_REFRESH_TOKEN = process.env.DBX_REFRESH_TOKEN;
